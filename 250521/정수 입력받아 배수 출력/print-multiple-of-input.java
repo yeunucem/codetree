f45c80d16a4b17e409b485841db7541ef6913f18
yeunucem <purplejy2331@gmail.com>
2025-05-21 19:23:53 +0900

    Title: 정수 입력받아 배수 출력, Time: 142ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250521/정수 입력받아 배수 출력/print-multiple-of-input.java
+++ 250521/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(system.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        System.out.printInt(n+' '+(n*2)+' '+(n*3)+' '+(n*4)+' '+(n*5));
-
-
+        System.out.print(n);
+        System.out.print(n*2);
+        System.out.print(n*3);
+        System.out.print(n*4);
+        System.out.print(n*5);
     }
 }
